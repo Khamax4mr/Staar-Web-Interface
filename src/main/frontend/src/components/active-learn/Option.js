2855812a46ca40737fd4c3f801c19c3e56064887
Khamax <hvinapark@gmail.com>
2024-11-01 20:59:53 +0900

    옵션 컴포넌트 추가.

---
+++ src/main/frontend/src/components/active-learn/Option.js
@@ -0,0 +1,90 @@
+import {FormControl, MenuItem} from '@mui/material';
+import {Container, Title} from '../../layouts/Frame';
+import {CheckBox, SelectBox, ShortTextBox, LongTextBox} from '../../layouts/Field';
+
+function OptFolder() {
+  return (
+    <Container>
+      <Title>프로그램 폴더</Title>
+      {/* Todo: 서버 파일 선택 구현 시 disabled 처리할 것. */}
+      <LongTextBox/>
+    </Container>
+  );
+}
+
+function OptModel() {
+  return (
+    <Container>
+      <Title>모델 코드</Title>
+      {/* Todo: 선택한 프로그램 폴더 따라 경로 설정. */}
+      <LongTextBox disabled/>
+    </Container>
+  );
+}
+
+function OptTrace() {
+  return (
+    <Container>
+      <Title>트레이스</Title>
+      {/* Todo: 선택한 프로그램 폴더 따라 경로 설정. */}
+      <LongTextBox disabled/>
+    </Container>
+  );
+}
+
+function OptTarget() {
+  return (
+    <Container>
+      <Title>관심 변수 이름</Title>
+      {/* Todo: 입력 형식 점검. */}
+      <ShortTextBox/>
+    </Container>
+  );
+}
+
+function OptWorkDir() {
+  return (
+    <Container>
+      <Title>작업 폴더</Title>
+      {/* Todo: 서버 파일 선택 구현 시 disabled 처리할 것. */}
+      <LongTextBox/>
+    </Container>
+  );
+}
+
+function OptBenchmark() {
+  return (
+    <Container>
+      <Title>benchmark</Title>
+      {/* Todo: 초기값 true로 설정. */}
+      <CheckBox/>
+    </Container>
+  );
+}
+
+function OptNoConst() {
+  return (
+    <Container>
+      <Title>no-const</Title>
+      {/* Todo: 초기값 true로 설정. */}
+      <CheckBox/>
+    </Container>
+  );
+}
+
+function OptTeacher() {
+  return (
+    <Container>
+      <Title>teacher</Title>
+      {/* Todo: 초기값 baseline으로 설정. */}
+      <FormControl>
+        <SelectBox>
+          <MenuItem value={'baseline'}>baseline</MenuItem>
+          <MenuItem value={'improved'}>improved</MenuItem>
+        </SelectBox>
+      </FormControl>
+    </Container>
+  );
+}
+
+export {OptFolder, OptModel, OptTrace, OptTarget, OptWorkDir, OptBenchmark, OptNoConst, OptTeacher};
